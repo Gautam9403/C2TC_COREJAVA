493649c230578383298d9caac94bdf555913744d
Gautam Mishra <Gautam Mishra@DESKTOP-6E36Q78>
2022-03-24 22:12:30 +0530

    Addition

---
+++ Day1/src/Addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Addition
+		
+	    int a = 50;
+		int b = 100;
+		int c = a + b;
+		
+		
+		 System.out.println("The sum of a and b is " +c);	
+	
+
+	}
+
+}
